5146f6941cb61cf5d54a7eff462c3f9c5eb1cabb
vitordcgomes <vitordcgomes@gmail.com>
2023-07-13 23:24:27 -0300

    lab14 top initial implementation

--- lab14/top14.vhd
+++ lab14/top14.vhd
@@ -26,14 +26,119 @@ entity top14 is
   
   Port (
     clk : in std_logic;
-    sw : in std_logic_vector (8 downto 0);
+    sw_prior : in std_logic_vector (15 downto 13);
+    sw : in std_logic_vector(4 downto 0);
     led : out std_logic_vector (3 downto 0)
    );
 
 end top14;
 
 architecture arch of top14 is
-begin
 
+  signal enable: std_logic;
+  signal cod_prio_out: std_logic_vector(1 downto 0);
+  signal mux_out, ffd_out, inc_4bits_out: std_logic_vector(3 downto 0);
+
+  begin
+    
+    --divisor de clock
+    div_clk_unity: entity work.div_clk
+    port map(
+      clk => clk,
+      en => enable
+    );
+
+    --codificador de prioridade
+    cod_prio_unity: entity work.cod_prio
+    port map(
+      r => sw_prior(15 downto 13),
+      pcode => cod_prio_out
+    );
+    
+    --Somador de 4 bits
+    inc_4bits_unity: entity work.inc_4bits
+    port map(
+      inc_in => ffd_out,
+      inc_out => inc_4bits_out
+    );
+
+
+    --primeiro bloco--------------------------------------------
+    mux_4x1_unity1: entity work.mux_4x1
+    port map(
+      c => cod_prio_out,
+      s => mux_out(0),
+      i(0) => ffd_out(0),
+      i(1) => ffd_out(1),
+      i(2) => inc_4bits_out(0),
+      i(3) => sw(0)
+    );
+
+    ffd_unity1: entity work.FF_D
+    port map(
+      clk => clk,
+      e => enable,
+      D => mux_out(0),
+      Q => ffd_out(0)
+    );
+
+    --segundo bloco-----------------------------------------------
+    mux_4x1_unity2: entity work.mux_4x1
+    port map(
+      c => cod_prio_out,
+      s => mux_out(0),
+      i(0) => ffd_out(0),
+      i(1) => ffd_out(1),
+      i(2) => inc_4bits_out(0),
+      i(3) => sw(0)
+    );
+
+    ffd_unity2: entity work.FF_D
+    port map(
+      clk => clk,
+      e => enable,
+      D => mux_out(0),
+      Q => ffd_out(0)
+    );
+
+    --terceiro bloco------------------------------------------------
+      mux_4x1_unity3: entity work.mux_4x1
+    port map(
+      c => cod_prio_out,
+      s => mux_out(0),
+      i(0) => ffd_out(0),
+      i(1) => ffd_out(1),
+      i(2) => inc_4bits_out(0),
+      i(3) => sw(0)
+    );
+
+    ffd_unity3: entity work.FF_D
+    port map(
+      clk => clk,
+      e => enable,
+      D => mux_out(0),
+      Q => ffd_out(0)
+    );
+    
+    --quarto bloco---------------------------------------------------
+     mux_4x1_unity4: entity work.mux_4x1
+    port map(
+      c => cod_prio_out,
+      s => mux_out(0),
+      i(0) => ffd_out(0),
+      i(1) => ffd_out(1),
+      i(2) => inc_4bits_out(0),
+      i(3) => sw(0)
+    );
+
+    ffd_unity4: entity work.FF_D
+    port map(
+      clk => clk,
+      e => enable,
+      D => mux_out(0),
+      Q => ffd_out(0)
+    );
+
+    led <= ffd_out; --leds recebem a saída dos flip flops
 
 end arch;
